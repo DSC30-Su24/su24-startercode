bab3b067fcac94f2868a185805b1c3ddb6294694
atobdura <atobdura@cs.washington.edu>
2024-08-06 23:40:11 -0700

    updated starter files

--- PA0/Node.java
+++ PA0/Node.java
@@ -1,7 +1,7 @@
 /**
 * TODO: add Javadoc class comment
 */
-public class Node {
+public class Node<T> {
     // TODO: Add necessary fields
 
     /**
@@ -14,7 +14,7 @@ public class Node {
     /**
     * TODO: add Javadoc comment
     */
-    private Node(T data, Node nextNode) {
+    private Node(T data, Node<T> nextNode) {
         // TODO: Your code here
     }
 
@@ -35,14 +35,14 @@ public class Node {
     /**
     * TODO: add Javadoc comment
     */
-    public void setNext(Node nextNode) {
+    public void setNext(Node<T> nextNode) {
         // TODO: Your code here
     }
 
     /**
     * TODO: add Javadoc comment
     */
-    public Node getNext() {
+    public Node<T> getNext() {
         // TODO: Your code here
     }
 
